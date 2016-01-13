a413e1884a9f3f4caf536ba6e10e2d64abd1b2d4
Alvaro Nistal <anistal@stratio.com>
2016-01-13 15:12:35 +0100

    created generic daos

---
+++ src/test/scala/com/stratio/common/integration/ZookeeperIntegrationTest.scala
@@ -0,0 +1,93 @@
+/**
+  * Copyright (C) 2015 Stratio (http://stratio.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+package com.stratio.common.integration
+
+import com.stratio.common.dao.DAOComponent
+import com.stratio.common.utils.config.TypesafeConfigComponent
+import com.stratio.common.utils.logger.Slf4jLoggerComponent
+import com.stratio.common.utils.repository.zookeeper.ZookeeperRepositoryComponent
+import org.apache.curator.test.TestingServer
+import org.apache.curator.utils.CloseableUtils
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+
+@RunWith(classOf[JUnitRunner])
+class ZookeeperIntegrationTest extends WordSpec
+  with Matchers
+  with BeforeAndAfterAll {
+
+  val ZookeeperTestPort: Int = 10666
+
+  var zkTestServer: TestingServer = _
+
+  def zookeeperStart: Unit = {
+    zkTestServer = new TestingServer(ZookeeperTestPort)
+    zkTestServer.start()
+  }
+
+  def zookeeperStop: Unit = {
+    CloseableUtils.closeQuietly(zkTestServer)
+    zkTestServer.stop()
+  }
+
+  override def beforeAll {
+    zookeeperStart
+  }
+
+  override def afterAll {
+    zookeeperStop
+  }
+
+  "A dao component" should {
+
+    "save a dummy in ZK and get it" in  new DummyDAOComponent {
+      dao.create("test1", new Dummy("value"))
+      dao.get("test1") should be(Some(Dummy("value")))
+    }
+
+    "update the dummy in ZK and get it" in  new DummyDAOComponent {
+      dao.update("test1", new Dummy("newValue"))
+      dao.get("test1") should be(Some(Dummy("newValue")))
+    }
+
+    "delete the dummy in ZK and get it with a None result" in  new DummyDAOComponent {
+      dao.delete("test1")
+      dao.exists("test1") should be(false)
+    }
+  }
+
+}
+
+trait DummyDAOComponent extends DAOComponent[String, Array[Byte], Dummy]
+  with ZookeeperRepositoryComponent with TypesafeConfigComponent with Slf4jLoggerComponent {
+
+  val dao: DAO = new DummyDAO {}
+
+  trait DummyDAO extends DAO {
+
+    def fromVtoM(v: Array[Byte]): Dummy = Dummy(new String(v))
+
+    def fromMtoV(m: Dummy): Array[Byte] = m.property.getBytes
+
+    //scalastyle:off
+    def entity= "dummy"
+    //scalastyle:on
+  }
+}
+
+case class Dummy(property: String)
---
+++ src/test/scala/com/stratio/common/repository/RepositoryComponentTest.scala
@@ -0,0 +1,109 @@
+/**
+  * Copyright (C) 2015 Stratio (http://stratio.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+package com.stratio.common.repository
+
+import com.stratio.common.utils.repository.DummyRepositoryComponent
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class RepositoryComponentTest extends WordSpec
+  with Matchers {
+
+  trait DummyRepositoryContext extends DummyRepositoryComponent
+
+  val keyNotFound = "-1"
+
+  val Entity = "dummy"
+
+  "A repository component" when {
+    "get an element" should {
+
+      "return an option with the value if the key exists" in new DummyRepositoryContext {
+        repository.get(Entity, "key1") should be(Some("value1"))
+      }
+
+      "return None if the key doesn't exist" in new DummyRepositoryContext {
+        repository.get(Entity, keyNotFound) should be(None)
+      }
+    }
+
+    "getAll elements" should {
+      "return all elements if the operation is successful" in new DummyRepositoryContext {
+        repository.getAll(Entity) should be(List("value1", "value2", "value3"))
+      }
+
+      "return and empty list if the operation is not successful" in new DummyRepositoryContext {
+        repository.getAll(keyNotFound) should be(List.empty[String])
+      }
+    }
+
+    "check if a value exists" should {
+
+      "return true if the value exists" in new DummyRepositoryContext {
+        repository.exists(Entity, "key1") should be(true)
+      }
+
+      "return false if the value doesn't exist" in new DummyRepositoryContext {
+        repository.exists(Entity, keyNotFound) should be(false)
+      }
+    }
+
+    "create a new value" should {
+
+      "return true if the operation is successful" in new DummyRepositoryContext {
+        repository.get(Entity, keyNotFound) should be(None)
+        repository.create(Entity, keyNotFound, "newValue") should be("newValue")
+        repository.get(Entity, keyNotFound) should be(Some("newValue"))
+      }
+
+      "return false if the operation is not successful" in new DummyRepositoryContext {
+        repository.create(Entity, "key1", "newValue") should be("newValue")
+        repository.count(Entity) should be(3)
+      }
+    }
+
+    "remove a value" should {
+
+      "check that the element does not exist when it has been removed" in new DummyRepositoryContext {
+        repository.get(Entity, "key1") should be(Some("value1"))
+        repository.delete(Entity, "key1")
+        repository.get(Entity, "key1") should be(None)
+      }
+
+      "do anything when a not existing element is removed" in new DummyRepositoryContext {
+        repository.delete(Entity, keyNotFound)
+        repository.count(Entity) should be(3)
+      }
+    }
+
+    "update a value" should {
+
+      "check that the element has been updated with the new value" in new DummyRepositoryContext {
+        repository.get(Entity, "key1") should be(Some("value1"))
+        repository.update(Entity, "key1", "newValue")
+        repository.get(Entity, "key1") should be(Some("newValue"))
+      }
+
+      "do anything when a not existing element is updated" in new DummyRepositoryContext {
+        repository.update(Entity, keyNotFound, "newValue")
+        repository.count(Entity) should be(3)
+      }
+    }
+  }
+}
